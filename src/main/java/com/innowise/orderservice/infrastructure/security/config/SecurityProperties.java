617777f82bea2433d98264473ca309a841fcaf6f
AbsoluteInDoubt <pervysbor@gmail.com>
2026-07-22 19:00:49 +0300

    feat: added actuator (not tested) due to kafka not configured in the compose

---
+++ src/main/java/com/innowise/orderservice/infrastructure/security/config/SecurityProperties.java
@@ -0,0 +1,17 @@
+package com.innowise.orderservice.infrastructure.security.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Set;
+
+@ConfigurationProperties(prefix = "application.security")
+public record SecurityProperties(
+        String jwksUrl,
+        Paths paths
+) {
+
+
+    public record Paths(
+            Set<String> publicPaths){}
+
+}
